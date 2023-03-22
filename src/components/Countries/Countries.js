6aba2932d41005d36b70509c38dd2ccca9891d7b
Hasib Hossain <boraborhasib@gmail.com>
2023-03-22 15:45:21 +0600

    Country Added

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
 
 const Countries = () => {
+    const [countries , setCountries ] = useState([])
+    useEffect(()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json()).then(data => setCountries(data))
+    } , [])
     return (
         <div>
-            <h1>Hello From Countries</h1>
+            {/* <h1>Country Name : <</h1> */}
+            {
+                countries.map(country => <Country name={country.name.common} area={country.area} population={country.population}></Country>)
+            }
         </div>
     );
 };
---
+++ src/components/Country/Country.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Country.css'
+const Country = (prop) => {
+    return (
+        <div className='country-box'>
+            <h1>Name : {prop.name}</h1>
+            <p>Area : {prop.area}</p>
+            <p><small>Population : {prop.population}</small></p>
+        </div>
+    );
+};
+
+export default Country;
